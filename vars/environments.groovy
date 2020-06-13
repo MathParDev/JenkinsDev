1b2e05543a2281f6f0569559cbe5f29c077dd037
Oleksii Retiznyk <OleksiiRetiznyk@gmail.com>
2020-06-13 20:14:24 +0300

    Added mathpar.com environment

--- vars/environments.groovy
+++ vars/environments.groovy
@@ -1,22 +1,25 @@
 def getList(){
-    return ['mathpar.ukma.edu.ua'];
+    return ['mathpar.ukma.edu.ua', 'mathpar.com'];
 }
 
 def getShortName(String fullName){
     return [
-            'mathpar.ukma.edu.ua': 'ukma'
+            'mathpar.ukma.edu.ua': 'ukma',
+            'mathpar.com': 'mathpar.com'
     ].get(fullName)
 }
 
 def getPropertiesFilename(String environment){
     return [
-            'mathpar.ukma.edu.ua': 'mathpar-ukma.json'
+            'mathpar.ukma.edu.ua': 'mathpar-ukma.json',
+            'mathpar.com': 'mathpar.json'
     ].get(environment)
 }
 
 def getHostString(String environment){
     return [
-            'mathpar.ukma.edu.ua': 'mathpar@mathpar.ukma.edu.ua'
+            'mathpar.ukma.edu.ua': 'mathpar@mathpar.ukma.edu.ua',
+            'mathpar.com': 'gennadi@mathpar.com'
     ].get(environment)
 }
 
